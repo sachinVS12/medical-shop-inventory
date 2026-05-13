1795596f2a3beda41873a68eeee12ed1870d3ad4
Sachin V S <vs1sachi12@gmail.com>
2026-05-13 11:13:48 +0530

    category import file in app api

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 // Dashboard route
 app.get("/api/dashboard", (req, res) => {
