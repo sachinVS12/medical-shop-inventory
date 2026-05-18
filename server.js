42aff1e6270ad651f036fc0aa4df3cb3fd289011
Sachin V S <vs1sachi12@gmail.com>
2026-05-18 16:28:46 +0530

    server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
